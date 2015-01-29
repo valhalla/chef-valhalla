ea61f3b6cbf8cef534064b2ae23773a885b3e1e7
Kevin Kreiser <kevinkreiser@gmail.com>
2015-01-29 12:52:08 -0500

    add a comment to test aws recipe push

--- recipes/retile.rb
+++ recipes/retile.rb
@@ -12,6 +12,7 @@ execute 'retile' do
   command 'echo "Re-tiling and then restarting server"'
 
   # TODO: write tiles to tmp location, then swap them in on server restart
+  #notifies :create,   'sometempdir[resource]',              'immediately'
   notifies :create,   'link[vertices config]',              :immediately
   notifies :create,   'link[edges config]',                 :immediately
   notifies :run,      'execute[cut tiles]',                 :immediately
