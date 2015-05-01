c51d6ae489f9c161ccb75c8e0f016112eb60079f
gknisely <greg@mapzen.com>
2015-05-01 09:40:51 -0400

    deploy logic in cron scripts now.

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -4,122 +4,15 @@
 # Recipe:: deploy
 #
 
-include_recipe 'valhalla::serve'
 include_recipe 'valhalla::data'
-
-# create new tiles with latest pbf and restart the server
-execute 'deploy' do
-  action  :nothing
-  command 'echo "Tiling with latest pbf and restarting server"'
-
-  # TODO: write tiles to tmp location, then swap them in on server restart
-  # notifies :create,  'execute[resource]',              'immediately'
-  notifies :create,   'link[vertices config]',              :immediately
-  notifies :create,   'link[edges config]',                 :immediately
-  notifies :create,   'link[admins config]',                :immediately
-  notifies :run,      'execute[update to mjolnir_tiles]',   :immediately
-  notifies :run,      'execute[clean mjolnir tiles]',       :immediately
-  notifies :run,      'execute[create admins]',             :immediately
-  notifies :run,      'execute[cut tiles]',                 :immediately
-  notifies :run,      'execute[clean tyr tiles]',           :immediately
-  notifies :run,      'execute[move tiles]',                :immediately
-  notifies :run,      'execute[clean up]',                  :immediately
-  # notifies :run,     'execute[publish data deficiencies]', :immediately
-  notifies :run,      'execute[update to tiles]',           :immediately
-  notifies :restart,  'runit_service[tyr-service]',         :immediately
-end
-
-# link the lua transforms from the checkout
-%w(vertices edges admins).each do |lua|
-  link "#{lua} config" do
-    action      :nothing
-    owner       node[:valhalla][:user][:name]
-    target_file "#{node[:valhalla][:conf_dir]}/#{lua}.lua"
-    to          "#{node[:valhalla][:src_dir]}/mjolnir/conf/#{lua}.lua"
-  end
-end
-
-# clean mjolnir tiles
-execute 'clean mjolnir tiles' do
-  action  :nothing
-  user    node[:valhalla][:user][:name]
-  command "rm -rf #{node[:valhalla][:mjolnir_tile_dir]}/*"
-  cwd     node[:valhalla][:base_dir]
-end
-
-# clean tyr tiles
-execute 'clean tyr tiles' do
-  action  :nothing
-  user    node[:valhalla][:user][:name]
-  command "rm -rf #{node[:valhalla][:tile_dir]}/*"
-  cwd     node[:valhalla][:base_dir]
-end
-
-# clean up
-execute 'clean up' do
-  action  :nothing
-  user    node[:valhalla][:user][:name]
-  command 'rm -rf *.bin'
-  cwd     node[:valhalla][:base_dir]
-end
-
-# move the newly created tiles to the tyr dir
-execute 'move tiles' do
-  action  :nothing
-  user    node[:valhalla][:user][:name]
-  command "mv #{node[:valhalla][:mjolnir_tile_dir]}/* #{node[:valhalla][:tile_dir]}/"
-  cwd     node[:valhalla][:base_dir]
-end
-
-# update tile dir in config to tiles
-execute 'update to tiles' do
-  action  :nothing
-  user    node[:valhalla][:user][:name]
-  command "sed -i 's/mjolnir_tiles/tiles/g' #{node[:valhalla][:config]}"
-  cwd     node[:valhalla][:conf_dir]
-end
-
-# update tile dir in config to mjolnir_tiles
-execute 'update to mjolnir_tiles' do
-  action  :nothing
-  user    node[:valhalla][:user][:name]
-  command "sed -i 's/tiles/mjolnir_tiles/g' #{node[:valhalla][:config]}"
-  cwd     node[:valhalla][:conf_dir]
-end
-
-# move the admin log
-execute 'move admin log' do
-  action  :nothing
-  user    node[:valhalla][:user][:name]
-  command "mv #{node[:valhalla][:log][:mjolnir]} #{node[:valhalla][:log][:mjolnir]}.admins"
-  cwd     node[:valhalla][:base_dir]
-end
 
 # the list of the files we will be importing
 files = node[:valhalla][:extracts].map { |url| url.split('/').last }
 extracts = node[:valhalla][:extracts_dir] + '/' + files.join(' ' + node[:valhalla][:extracts_dir] + '/')
 
-# create the admins from the data
-# TODO:  This will be removed when we thread admins in mjolnir.
-execute 'create admins' do
-  action   :nothing
-  user     node[:valhalla][:user][:name]
-  command  "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib pbfadminbuilder -c \
-           #{node[:valhalla][:conf_dir]}/#{node[:valhalla][:config]} #{extracts}"
-  cwd      node[:valhalla][:base_dir]
-  timeout  node[:valhalla][:tiles][:cut_tiles_timeout]
-  notifies :run,      'execute[move admin log]',                :immediately
+cron "add deploy cron" do
+  minute "*/5"
+  user "#{node[:valhalla][:user][:name]}"
+  command "cd #{node[:valhalla][:base_dir]}; #{node[:valhalla][:src_dir]}/mjolnir/scripts/deploy.sh #{node[:valhalla][:base_dir]} #{node[:valhalla][:conf_dir]}/#{node[:valhalla][:config]} #{node[:valhalla][:src_dir]} #{node[:valhalla][:extracts_dir]} #{node[:valhalla][:log][:mjolnir]} >> #{node[:valhalla][:log_dir]}/deploy_cron.log 2>&1"
 end
 
-# cut tiles from the data
-execute 'cut tiles' do
-  action   :nothing
-  user     node[:valhalla][:user][:name]
-  command  "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib pbfgraphbuilder -c \
-           #{node[:valhalla][:conf_dir]}/#{node[:valhalla][:config]} #{extracts}"
-  cwd      node[:valhalla][:base_dir]
-  timeout  node[:valhalla][:tiles][:cut_tiles_timeout]
-end
-
-# TODO: add an execute for publishing the possible data issues to maproulette
-# or some other such data correction facility
