81c02ef52836599a01908c8c3cd0ae13c9e220df
gknisely <greg@mapzen.com>
2015-05-01 09:50:05 -0400

    removed double quotes.

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -10,8 +10,8 @@ include_recipe 'valhalla::data'
 files = node[:valhalla][:extracts].map { |url| url.split('/').last }
 extracts = node[:valhalla][:extracts_dir] + '/' + files.join(' ' + node[:valhalla][:extracts_dir] + '/')
 
-cron "add deploy cron" do
-  minute "*/5"
+cron 'add deploy cron' do
+  minute '*/5'
   user "#{node[:valhalla][:user][:name]}"
   command "cd #{node[:valhalla][:base_dir]}; #{node[:valhalla][:src_dir]}/mjolnir/scripts/deploy.sh #{node[:valhalla][:base_dir]} #{node[:valhalla][:conf_dir]}/#{node[:valhalla][:config]} #{node[:valhalla][:src_dir]} #{node[:valhalla][:extracts_dir]} #{node[:valhalla][:log][:mjolnir]} >> #{node[:valhalla][:log_dir]}/deploy_cron.log 2>&1"
 end
