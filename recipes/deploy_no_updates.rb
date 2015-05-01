2366c897f6e21bd1fdea7ddfc6f0aa7e75cabec8
gknisely <greg@mapzen.com>
2015-05-01 10:17:24 -0400

    cleanup

--- recipes/deploy_no_updates.rb
+++ recipes/deploy_no_updates.rb
@@ -4,13 +4,11 @@
 # Recipe:: deploy_no_updates
 #
 
-# the list of the files we will be importing
-files = node[:valhalla][:extracts].map { |url| url.split('/').last }
-extracts = node[:valhalla][:extracts_dir] + '/' + files.join(' ' + node[:valhalla][:extracts_dir] + '/')
-
 cron 'add deploy cron' do
   minute '*/5'
   user "#{node[:valhalla][:user][:name]}"
-  command "cd #{node[:valhalla][:base_dir]}; #{node[:valhalla][:src_dir]}/mjolnir/scripts/deploy_no_updates.sh #{node[:valhalla][:base_dir]} #{node[:valhalla][:conf_dir]}/#{node[:valhalla][:config]} #{node[:valhalla][:src_dir]} #{node[:valhalla][:extracts_dir]} #{node[:valhalla][:log][:mjolnir]} >> #{node[:valhalla][:log_dir]}/deploy_cron.log 2>&1"
+  command "cd #{node[:valhalla][:base_dir]}; #{node[:valhalla][:src_dir]}/mjolnir/scripts/deploy_no_updates.sh \
+#{node[:valhalla][:base_dir]} #{node[:valhalla][:conf_dir]}/#{node[:valhalla][:config]} \
+#{node[:valhalla][:src_dir]} #{node[:valhalla][:extracts_dir]} #{node[:valhalla][:log][:mjolnir]} \
+>> #{node[:valhalla][:log_dir]}/deploy_no_updates_cron.log 2>&1"
 end
-
