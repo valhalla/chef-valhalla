4a45ab2f1e72ea2c824ba93633c6d698c7b52d76
Kevin Kreiser <kevinkreiser@gmail.com>
2015-05-14 16:02:49 -0400

    split out the serve recipe from single point of failure to more redundant system

--- recipes/serve.rb
+++ recipes/serve.rb
@@ -4,8 +4,8 @@
 # Recipe:: serve
 #
 
-# restart the service if all is well
-runit_service 'tyr-service' do
+# httpd
+ruint_service 'prime-httpd' do
   action          :enable
   log             true
   default_logger  true
@@ -14,4 +14,33 @@ runit_service 'tyr-service' do
   env(
     'LD_LIBRARY_PATH' => '/usr/lib:/usr/local/lib'
   )
+end
+
+# cake layers
+%w(loki thor odin tyr).each do |layer|
+  #proxy
+  runit_service "proxyd-#{layer}" do
+    action          :enable
+    log             true
+    default_logger  true
+    sv_timeout      60
+    retries         3
+    env(
+      'LD_LIBRARY_PATH' => '/usr/lib:/usr/local/lib'
+    )
+  end
+
+  #workers
+  %w(0 1 2 3 4 5 6 7).each do |n|
+    runit_service "workerd-#{layer}-#{n}" do
+      action          :enable
+      log             true
+      default_logger  true
+      sv_timeout      60
+      retries         3
+      env(
+        'LD_LIBRARY_PATH' => '/usr/lib:/usr/local/lib'
+      )
+    end
+  end
 end
