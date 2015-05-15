3b11a49d9c328d9bd0f59cde5496fe663ae7a5ee
Grant Heffernan <heffergm@gmail.com>
2015-05-15 09:09:21 -0400

    - fix attribute in various sv template: [:valhalla][:config_dir] =>   [:valhalla][:conf_dir] (config_dir is not an existing attribute) - use a common template where possible for services

--- recipes/serve.rb
+++ recipes/serve.rb
@@ -4,6 +4,8 @@
 # Recipe:: serve
 #
 
+include_recipe 'runit::default'
+
 # httpd
 runit_service 'prime-httpd' do
   action          :enable
@@ -20,27 +22,27 @@ end
 %w(loki thor odin tyr).each do |layer|
   # proxy
   runit_service "proxyd-#{layer}" do
-    action          :enable
-    log             true
-    default_logger  true
-    sv_timeout      60
-    retries         3
-    env(
-      'LD_LIBRARY_PATH' => '/usr/lib:/usr/local/lib'
-    )
+    action            :enable
+    log               true
+    default_logger    true
+    run_template_name 'proxyd-global-run'
+    sv_timeout        60
+    retries           3
+    options(layer: layer)
+    env('LD_LIBRARY_PATH' => '/usr/lib:/usr/local/lib')
   end
 
   # workers
-  %w(0 1 2 3 4 5 6 7).each do |n|
-    runit_service "workerd-#{layer}-#{n}" do
-      action          :enable
-      log             true
-      default_logger  true
-      sv_timeout      60
-      retries         3
-      env(
-        'LD_LIBRARY_PATH' => '/usr/lib:/usr/local/lib'
-      )
+  (0..7).step(1).each do |num|
+    runit_service "workerd-#{layer}-#{num}" do
+      action            :enable
+      log               true
+      default_logger    true
+      run_template_name 'workerd-global-run'
+      sv_timeout        60
+      retries           3
+      options(layer: layer, num: num)
+      env('LD_LIBRARY_PATH' => '/usr/lib:/usr/local/lib')
     end
   end
 end
