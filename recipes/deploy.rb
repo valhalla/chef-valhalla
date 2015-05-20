b40f3c90734e9be8df48a05ea7fb87356898e3d0
Kevin Kreiser <kevinkreiser@gmail.com>
2015-05-20 16:44:49 -0400

    revert log stash stuff will move to a wrapper cookbook

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -6,7 +6,7 @@
 
 cron 'add deploy cron' do
   minute '*/5'
-  user node[:valhalla][:user][:name]
+  user "#{node[:valhalla][:user][:name]}"
   command "cd #{node[:valhalla][:base_dir]}; #{node[:valhalla][:src_dir]}/mjolnir/scripts/deploy.sh \
 #{node[:valhalla][:base_dir]} #{node[:valhalla][:conf_dir]}/#{node[:valhalla][:config]} \
 #{node[:valhalla][:src_dir]} #{node[:valhalla][:extracts_dir]} #{node[:valhalla][:log][:mjolnir]} \
--- recipes/logstash.rb
+++
@@ -1,6 +0,0 @@
-include_recipe 'mapzen_logstash::default'
-
-logstashrc 'logstash_valhalla' do
-  template_source   'logstash_valhalla.conf.erb'
-  template_cookbook 'valhalla'
-end
