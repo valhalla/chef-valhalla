ef93cd7f627a1d6e5878d73155889043d94b4858
Kevin Kreiser <kevinkreiser@gmail.com>
2015-06-02 12:23:12 -0400

    yeah apparently runit services are question mark which to go stderr

--- recipes/_restart.rb
+++ recipes/_restart.rb
@@ -9,7 +9,7 @@ include_recipe 'runit::default'
 # httpd
 runit_service 'prime-httpd' do
   action          :restart
-  only_if         "test $(service --status-all | grep -cF 'prime-httpd') = 1"
+  only_if         "test $(service --status-all 2>&1 | grep -cF 'prime-httpd') = 1"
 end
 
 # cake layers
@@ -17,14 +17,14 @@ end
   # proxy
   runit_service "proxyd-#{layer}" do
     action            :restart
-    only_if         "test $(service --status-all | grep -cF 'proxyd-#{layer}') = 1"
+    only_if         "test $(service --status-all 2>&1 | grep -cF 'proxyd-#{layer}') = 1"
   end
 
   # workers
   (0..(node[:valhalla][:workers][:count] - 1)).step(1).each do |num|
     runit_service "workerd-#{layer}-#{num}" do
       action            :restart
-      only_if         "test $(service --status-all | grep -cF 'workerd-#{layer}-#{num}') = 1"
+      only_if         "test $(service --status-all 2>&1 | grep -cF 'workerd-#{layer}-#{num}') = 1"
     end
   end
 end
