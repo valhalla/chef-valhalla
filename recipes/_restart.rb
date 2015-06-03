d9c6ea8b27651b6052df2d3d27b9af891faf5e19
Grant Heffernan <heffergm@gmail.com>
2015-06-03 16:52:12 -0400

    lint

--- recipes/_restart.rb
+++ recipes/_restart.rb
@@ -8,23 +8,23 @@ include_recipe 'runit::default'
 
 # httpd
 runit_service 'prime-httpd' do
-  action          :restart
-  only_if         "test $(service --status-all 2>&1 | grep -cF 'prime-httpd') = 1"
+  action  :restart
+  only_if "test $(service --status-all 2>&1 | grep -cF 'prime-httpd') = 1"
 end
 
 # cake layers
 %w(loki thor odin tyr).each do |layer|
   # proxy
   runit_service "proxyd-#{layer}" do
-    action            :restart
-    only_if         "test $(service --status-all 2>&1 | grep -cF 'proxyd-#{layer}') = 1"
+    action  :restart
+    only_if "test $(service --status-all 2>&1 | grep -cF 'proxyd-#{layer}') = 1"
   end
 
   # workers
   (0..(node[:valhalla][:workers][:count] - 1)).step(1).each do |num|
     runit_service "workerd-#{layer}-#{num}" do
-      action            :restart
-      only_if         "test $(service --status-all 2>&1 | grep -cF 'workerd-#{layer}-#{num}') = 1"
+      action  :restart
+      only_if "test $(service --status-all 2>&1 | grep -cF 'workerd-#{layer}-#{num}') = 1"
     end
   end
 end
