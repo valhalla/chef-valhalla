b4fc7ce4718a94b3b2fbae11d883d2eae001627e
Kevin Kreiser <kevinkreiser@gmail.com>
2015-06-04 14:42:41 -0400

    let the minutely updates happen all the time. moving tiles to s3 happens in the background. silence and alrady initialzed diff process

--- recipes/cut_tiles.rb
+++ recipes/cut_tiles.rb
@@ -4,7 +4,7 @@
 # Recipe:: cut_tiles
 #
 
-# use a script to either cut tiles as a one-off or install crontab to do it all the time
+# cut tiles as a one-off
 execute 'cut tiles' do
   user    node[:valhalla][:user][:name]
   cwd     node[:valhalla][:base_dir]
@@ -14,11 +14,25 @@ execute 'cut tiles' do
   only_if { node[:valhalla][:with_updates] == false }
 end
 
+# or install crontab to cut tiles all the time
 cron 'cut tiles' do
   user    node[:valhalla][:user][:name]
   minute  '*/5'
   command <<-EOH
-    cd #{node[:valhalla][:base_dir]} && #{node[:valhalla][:conf_dir]}/cut_tiles.sh >>#{node[:valhalla][:log_dir]}/cut_tiles.log 2>&1
+    cd #{node[:valhalla][:base_dir]} && #{node[:valhalla][:conf_dir]}/cut_tiles.sh >> #{node[:valhalla][:log_dir]}/cut_tiles.log 2>&1
   EOH
   only_if { node[:valhalla][:with_updates] == true }
+end
+
+
+# make sure to keep the data up to date if we are constantly cutting tiles
+node[:valhalla][:extracts].each do |file|
+  cron "apply changeset #{file}" do
+    user    node[:valhalla][:user][:name]
+    minute  '*/5'
+    command <<-EOH
+      cd #{node[:valhalla][:base_dir]} && #{node[:valhalla][:conf_dir]}/minutely_update.sh update #{node[:valhalla][:extracts_dir]} #{file} >> #{node[:valhalla][:log_dir]}/minutely_update_#{file}.log 2>&1
+    EOH
+    only_if { node[:valhalla][:with_updates] == true }
+  end
 end
