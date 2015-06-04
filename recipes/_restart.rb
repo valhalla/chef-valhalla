0a73a2c6f198ff67fc33b2a1b0c08a7cc5210d61
Grant Heffernan <heffergm@gmail.com>
2015-06-04 14:31:19 -0400

    check links in /etc/service instead

--- recipes/_restart.rb
+++ recipes/_restart.rb
@@ -9,7 +9,7 @@ include_recipe 'runit::default'
 # httpd
 runit_service 'prime-httpd' do
   action  :restart
-  only_if "test -d #{node[:runit][:sv_dir]}/prime-httpd"
+  only_if "test -h #{node[:runit][:service_dir]}/prime-httpd"
 end
 
 # cake layers
@@ -17,14 +17,14 @@ end
   # proxy
   runit_service "proxyd-#{layer}" do
     action  :restart
-    only_if "test -d #{node[:runit][:sv_dir]}/proxyd-#{layer}"
+    only_if "test -h #{node[:runit][:service_dir]}/proxyd-#{layer}"
   end
 
   # workers
   (0..(node[:valhalla][:workers][:count] - 1)).step(1).each do |num|
     runit_service "workerd-#{layer}-#{num}" do
       action  :restart
-      only_if "test -d #{node[:runit][:sv_dir]}/workerd-#{layer}-#{num}"
+      only_if "test -h #{node[:runit][:service_dir]}/workerd-#{layer}-#{num}"
     end
   end
 end
