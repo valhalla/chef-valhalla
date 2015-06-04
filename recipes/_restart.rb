399c7d269b9bb4f5c60aa43e33da8c3a0741263c
Grant Heffernan <heffergm@gmail.com>
2015-06-04 14:27:22 -0400

    faster test

--- recipes/_restart.rb
+++ recipes/_restart.rb
@@ -9,7 +9,7 @@ include_recipe 'runit::default'
 # httpd
 runit_service 'prime-httpd' do
   action  :restart
-  only_if "test $(service --status-all 2>&1 | grep -cF 'prime-httpd') = 1"
+  only_if "test -d #{node[:runit][:sv_dir]}/prime-httpd"
 end
 
 # cake layers
@@ -17,14 +17,14 @@ end
   # proxy
   runit_service "proxyd-#{layer}" do
     action  :restart
-    only_if "test $(service --status-all 2>&1 | grep -cF 'proxyd-#{layer}') = 1"
+    only_if "test -d #{node[:runit][:sv_dir]}/proxyd-#{layer}"
   end
 
   # workers
   (0..(node[:valhalla][:workers][:count] - 1)).step(1).each do |num|
     runit_service "workerd-#{layer}-#{num}" do
       action  :restart
-      only_if "test $(service --status-all 2>&1 | grep -cF 'workerd-#{layer}-#{num}') = 1"
+      only_if "test -d #{node[:runit][:sv_dir]}/workerd-#{layer}-#{num}"
     end
   end
 end
