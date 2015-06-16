f846ee66453a2c7fa9a961795da168bc8dcd70f6
Grant Heffernan <heffergm@gmail.com>
2015-06-16 13:43:07 -0400

    0.4.4

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'valhalla@mapzen.com'
 license          'MIT'
 description      'Installs/Configures valhalla'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.3'
+version          '0.4.4'
 
 recipe 'valhalla', 'Installs valhalla'
 
