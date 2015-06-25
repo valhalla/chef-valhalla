91f5789c04a6012c8ae464050d7804fb8c97e93e
Kevin Kreiser <kevinkreiser@gmail.com>
2015-06-25 14:55:58 -0400

    cowboy baby

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'valhalla@mapzen.com'
 license          'MIT'
 description      'Installs/Configures valhalla'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.4'
+version          '0.4.5'
 
 recipe 'valhalla', 'Installs valhalla'
 
