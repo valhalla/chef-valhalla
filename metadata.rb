0a0f96bd8b79cbfbaa679b3aa16443da096e0233
Kevin Kreiser <kevinkreiser@gmail.com>
2015-07-21 13:43:34 -0400

    update version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'valhalla@mapzen.com'
 license          'MIT'
 description      'Installs/Configures valhalla'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.1'
+version          '0.6.0'
 
 recipe 'valhalla', 'Installs valhalla'
 
