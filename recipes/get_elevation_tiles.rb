c5ed0ff3567f0aee4a288425aa93ec27690ea604
Kevin Kreiser <kevinkreiser@gmail.com>
2015-08-25 11:26:39 -0400

    forward output to log

--- recipes/get_elevation_tiles.rb
+++ recipes/get_elevation_tiles.rb
@@ -16,7 +16,7 @@ execute 'sync tiles' do
   action  :run
   user    node[:valhalla][:user][:name]
   cwd     node[:valhalla][:src_dir]
-  command "skadi/scripts/elevation_extract.sh -180 180 -90 90 #{node[:valhalla][:elevation_dir]} $(($(nproc)*2))"
+  command "skadi/scripts/elevation_extract.sh -180 180 -90 90 #{node[:valhalla][:elevation_dir]} $(($(nproc)*2)) 2>&1 >> #{node[:valhalla][:log_dir]}/elevation_tiles.log"
   retries 3
   timeout 32_000
 end
