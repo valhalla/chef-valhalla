28556f1cf187d9cf29f1731d42fbf83fea2d5e8f
Kevin Kreiser <kevinkreiser@gmail.com>
2015-09-25 11:33:51 -0400

    add back convenient restart recipe

---
+++ recipes/_restart.rb
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+#
+# Cookbook Name:: valhalla
+# Recipe:: _restart
+#
+
+#stop everything from running
+include_recipe 'runit::default'
+stop_service do
+end
+
+# turn everything back on
+start_service do
+end
