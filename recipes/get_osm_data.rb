c6e523cc779028630cb4c5f385bc7d9a450d9665
Kevin Kreiser <kevinkreiser@gmail.com>
2015-12-15 11:05:18 -0500

    reverse order and use negation operator

--- recipes/get_osm_data.rb
+++ recipes/get_osm_data.rb
@@ -15,7 +15,7 @@ node[:valhalla][:extracts].each do |url|
     backup   false
     source   "#{url}.md5"
     mode     0644
-    only_if  { node[:valhalla][:with_updates] == false || File.exist?("#{node[:valhalla][:extracts_dir]}/#{file}") == false }
+    only_if  { !File.exist?("#{node[:valhalla][:extracts_dir]}/#{file}") || !node[:valhalla][:with_updates] }
 
     notifies :run, "execute[download #{url}]", :immediately
     notifies :run, "ruby_block[verify #{file}]", :immediately
