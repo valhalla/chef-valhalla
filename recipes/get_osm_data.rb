74a48fff1cac4121150da420821e3f71f48a6a0c
Kevin Kreiser <kevinkreiser@gmail.com>
2015-12-15 10:58:59 -0500

    other or

--- recipes/get_osm_data.rb
+++ recipes/get_osm_data.rb
@@ -15,7 +15,7 @@ node[:valhalla][:extracts].each do |url|
     backup   false
     source   "#{url}.md5"
     mode     0644
-    only_if  { node[:valhalla][:with_updates] == false or File.exist?("#{node[:valhalla][:extracts_dir]}/#{file}") == false }
+    only_if  { node[:valhalla][:with_updates] == false || File.exist?("#{node[:valhalla][:extracts_dir]}/#{file}") == false }
 
     notifies :run, "execute[download #{url}]", :immediately
     notifies :run, "ruby_block[verify #{file}]", :immediately
