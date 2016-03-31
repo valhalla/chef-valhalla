ef7785f4410cf2589ab906f43a4150345026bf19
kdiluca <kristen@mapzen.com>
2016-03-31 15:53:05 -0400

    added optimized action for running in dev environment

--- attributes/default.rb
+++ attributes/default.rb
@@ -59,7 +59,7 @@ else
   default[:valhalla][:max_cache_size]                            = "#{((node.memory.total.to_f / (node.cpu.total.to_f * 2)) * 0.9).floor * 1024}"
 end
 if !node[:opsworks]
-  default[:valhalla][:actions]                                   = '["locate","route","one_to_many","many_to_one","many_to_many"]'
+  default[:valhalla][:actions]                                   = '["locate","route","one_to_many","many_to_one","many_to_many", "optimized"]'
 elsif node[:opsworks][:layers][:'matrix'] && node[:opsworks][:instance][:layers].include?('matrix')
   default[:valhalla][:actions]                                   = '["one_to_many","many_to_one","many_to_many"]'
 else
