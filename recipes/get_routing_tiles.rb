8b3929496f7dd415b777f90a4dc49ada62832735
Kevin Kreiser <kevinkreiser@gmail.com>
2016-10-17 13:15:39 -0400

    use mmap tar of planet tiles. use aws cli where possible always export edges (#129)
    * fixes #128. use mmap tar of planet tiles. use aws cli where possible. always export edges.
    
    * change config
    
    * changed then ame of the tiles
    
    * update version

--- recipes/get_routing_tiles.rb
+++ recipes/get_routing_tiles.rb
@@ -4,44 +4,17 @@
 # Recipe:: get_routing_tiles
 #
 
-# stop everything from running
+# stop everything from running, while we get new tiles
 include_recipe 'runit::default'
 stop_service do
-  notifies :run, 'execute[pull tiles]', :immediately
-  notifies :run, 'execute[extract tiles]', :immediately
-  notifies :run, 'execute[move tiles]', :immediately
-end
-
-# go get the tiles
-execute 'pull tiles' do
   action  :run
   user    node[:valhalla][:user][:name]
   cwd     node[:valhalla][:base_dir]
   command <<-EOH
-    echo -n https://s3.amazonaws.com/#{node[:valhalla][:bucket]}/#{node[:valhalla][:bucket_dir]}/ > latest_tiles.txt &&
-    aws --region us-east-1 s3 ls s3://#{node[:valhalla][:bucket]}/#{node[:valhalla][:bucket_dir]}/ | grep -F tiles_ | awk '{print $4}' | sort | tail -n 1 >> latest_tiles.txt
+    echo -n s3://#{node[:valhalla][:bucket]}/#{node[:valhalla][:bucket_dir]}/ > latest_tiles.txt &&
+    aws --region us-east-1 s3 ls $(cat latest_tiles.txt) | grep -F planet_ | awk '{print $4}' | sort | tail -n 1 >> latest_tiles.txt &&
+    aws --region us-east-1 s3 cp $(cat latest_tiles.txt) .
   EOH
-end
-
-# open them up
-execute 'extract tiles' do
-  action  :run
-  user    node[:valhalla][:user][:name]
-  cwd     node[:valhalla][:base_dir]
-  command <<-EOH
-    rm -rf tmp_tiles old_tiles &&
-    mkdir tmp_tiles &&
-    curl $(cat latest_tiles.txt) 2>#{node[:valhalla][:log_dir]}/curl_tiles.log | tar xzp -C tmp_tiles 2>#{node[:valhalla][:log_dir]}/untar_tiles.log
-  EOH
-  retries 3
-end
-
-# move them into place
-execute 'move tiles' do
-  action  :run
-  user    node[:valhalla][:user][:name]
-  command "mv #{node[:valhalla][:tile_dir]} old_tiles; mv tmp_tiles #{node[:valhalla][:tile_dir]}"
-  cwd     node[:valhalla][:base_dir]
 end
 
 # turn everything back on
