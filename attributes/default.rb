c63bc381c8d90438ff64550eecf1bd51be45e08d
kdiluca <kristen@mapzen.com>
2017-01-30 12:50:10 -0500

    added trace actions

--- attributes/default.rb
+++ attributes/default.rb
@@ -74,11 +74,11 @@ else
   default[:valhalla][:max_cache_size]                            = "#{((node.memory.total.to_f / (node.cpu.total.to_f * 2)) * 0.9).floor * 1024}"
 end
 if !node[:opsworks]
-  default[:valhalla][:actions]                                   = '["locate","route","one_to_many","many_to_one","many_to_many","optimized_route","isochrone","sources_to_targets"]'
+  default[:valhalla][:actions]                                   = '["locate","route","one_to_many","many_to_one","many_to_many","optimized_route","isochrone","sources_to_targets","trace_route","trace_attributes"]'
 elsif node[:opsworks][:layers][:'matrix'] && node[:opsworks][:instance][:layers].include?('matrix')
   default[:valhalla][:actions]                                   = '["one_to_many","many_to_one","many_to_many","optimized_route","isochrone","sources_to_targets"]'
 else
-  default[:valhalla][:actions]                                   = '["locate","route"]'
+  default[:valhalla][:actions]                                   = '["locate","route","trace_route","trace_attributes"]'
 end
 default[:valhalla][:elevation][:actions]                         = '["height"]'
 default[:valhalla][:mjolnir][:concurrency]                       = node[:cpu][:total]
