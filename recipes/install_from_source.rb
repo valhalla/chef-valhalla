a954181bc7d3dbe7ef1dbdfd12314681697fe2af
Kevin Kreiser <kevinkreiser@gmail.com>
2017-02-03 15:18:14 -0500

    easier source install

--- recipes/install_from_source.rb
+++ recipes/install_from_source.rb
@@ -10,24 +10,22 @@ execute 'package remove' do
   command '(apt-get purge -y libvalhalla* valhalla* || true) && rm -rf /usr/local/lib/libvalhalla* /usr/local/include/valhalla /usr/local/bin/valhalla*'
 end
 
-%w(midgard baldr sif meili skadi mjolnir loki odin thor tyr tools).each do |layer|
-  # clone software
-  execute "clone #{layer}" do
-    action    :run
-    command   "rm -rf #{layer} && git clone --depth=1 --recurse-submodules --single-branch \
-              --branch=#{node[:valhalla][:github][:revision]} \
-              #{node[:valhalla][:github][:base]}/#{layer}.git"
-    cwd       node[:valhalla][:src_dir]
+# clone software
+execute "clone libvalhalla" do
+  action    :run
+  command   "rm -rf libvalhalla && git clone --depth=1 --recurse-submodules --single-branch \
+            --branch=#{node[:valhalla][:github][:revision]} \
+            #{node[:valhalla][:github][:base]}/libvalhalla.git"
+  cwd       node[:valhalla][:src_dir]
 
-    notifies  :run, "execute[install #{layer}]",       :immediately
-  end
+  notifies  :run, "execute[install libvalhalla]",       :immediately
+end
 
-  # install
-  execute "install #{layer}" do
-    action  :nothing
-    command 'scripts/install.sh'
-    cwd     "#{node[:valhalla][:src_dir]}/#{layer}"
-  end
+# install
+execute "install libvalhalla" do
+  action  :nothing
+  command 'scripts/install.sh'
+  cwd     "#{node[:valhalla][:src_dir]}/libvalhalla"
 end
 
 # restart the services if they are present
