6395008a21ef13fbee64dceca365356d38b61f6d
kdiluca <kristen@mapzen.com>
2017-03-09 11:35:16 -0500

    Updated metadata version to 1.0.1
    includes avoid locations service limit and meili pedestrian search radius update

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'valhalla@mapzen.com'
 license          'MIT'
 description      'Installs/Configures valhalla'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 recipe 'valhalla', 'Installs valhalla'
 
